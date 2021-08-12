b83955c0adf5ba1e2082774615fa11c7871def56
lukelovekin <lukelovekin@gmail.com>
2021-08-12 21:27:34 +1000

    tcp socket

--- client.py
+++ client.py
@@ -1,9 +1,10 @@
 from socket import *
 serverName = '127.0.0.1'
 serverPort = 11500
-clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket = socket(AF_INET, SOCK_STREAM)
 message = "Hello Joe"
+clientSocket.connect((serverName, serverPort))
 clientSocket.sendto(message.encode(), (serverName, serverPort))
-ServerReply, serverAddress = clientSocket.recvfrom(2048)
+ServerReply = clientSocket.recv(1024)
 print(ServerReply.decode())
 clientSocket.close()
--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from socket import *
 serverPort = 11500
-serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(("", serverPort))
+serverSocket.listen(1)
 print("The server is listening")
 while True:
-    message, clientAddress = serverSocket.recvfrom(2048)
-    ClientSendMessage1 = str(len(str(message))) + " " +  str(message).upper()
-    serverSocket.sendto(ClientSendMessage1.encode(), clientAddress)
+    connectCl1Socket, addr = serverSocket.accept()
+    data = connectCl1Socket.recv(1024)
+    ClientSendMessage1 = str(len(str(data.decode()))) + " " +  str(data.decode()).upper()
+    connectCl1Socket.sendall(ClientSendMessage1.encode())
 
