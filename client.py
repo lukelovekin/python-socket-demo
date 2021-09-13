be1668a086c9f2a4eb67407abcd398aea970f548
lukelovekin <lukelovekin@gmail.com>
2021-09-13 20:45:31 +1000

    socket udp chat

--- client.py
+++ client.py
@@ -2,8 +2,20 @@ from socket import *
 serverName = '127.0.0.1'
 serverPort = 11500
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = "Hello Joe"
+
+# Hello message send
+message = "Hello"
 clientSocket.sendto(message.encode(), (serverName, serverPort))
 ServerReply, serverAddress = clientSocket.recvfrom(2048)
+
 print(ServerReply.decode())
+
+# Name response
+message  = input()
+clientSocket.sendto(message.encode(), (serverName, serverPort))
+ServerReply, serverAddress = clientSocket.recvfrom(2048)
+
+print(ServerReply.decode())
+
+
 clientSocket.close()
--- server.py
+++ server.py
@@ -5,6 +5,14 @@ serverSocket.bind(("", serverPort))
 print("The server is listening")
 while True:
     message, clientAddress = serverSocket.recvfrom(2048)
-    ClientSendMessage1 = str(len(str(message))) + " " +  str(message).upper()
-    serverSocket.sendto(ClientSendMessage1.encode(), clientAddress)
+    if message.decode('utf-8') == 'Hello':
+        # ClientSendMessage1 = str(len(str(message))) + " " +  str(message).upper()
+        print(message.decode('utf-8'))
+        ClientSendMessage1 = "Hello, Whats your name?"
+        serverSocket.sendto(ClientSendMessage1.encode(), clientAddress)
+    else: 
+        print(message.decode('utf-8'))
+        ClientSendMessage2 = f'Hello {message.decode("utf-8")}, welcome to SIT202'
+        serverSocket.sendto(ClientSendMessage2.encode(), clientAddress)
+
 
